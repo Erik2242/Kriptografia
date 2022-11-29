d497efe17180a771ac83f7e25ef16fedb5e3a21f
Erik224 <erik.horvath200@gmail.com>
2022-11-29 15:26:53 +0200

    implemented solitare algorithm

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,4 +1,5 @@
-from random import seed, randint
+from random import seed, randint, shuffle
+from solitare import Solitare
 
 
 def encrypt(text, key):
@@ -15,10 +16,16 @@ def decrypt(text, key):
         result = result + chr((ord(letter) + 255 - randint(0, 255)) % 255 )
     return result
 
-
 def main():
-    res = encrypt("AbraCada371nd!';]", "ami")
-    print(decrypt(res, "ami"))
+    # res = encrypt("AbraCada371nd!';]", "ami")
+    # print(decrypt(res, "ami"))
+    deck = list(range(1,55))
+    shuffle(deck)
+    solitare1 = Solitare(deck.copy())
+    solitare2 = Solitare(deck.copy())
+    print(solitare1.shuffle())
+    print(solitare2.shuffle())
+    
 
 
 if __name__ == "__main__":
